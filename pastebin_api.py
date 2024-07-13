f28a8bab95128ff39a78cda2436447018190523a
Joelle Waugh <joelle.waugh@flemingcollege.ca>
2024-07-12 23:52:29 -0400

    Pokemon API code

---
+++ pastebin_api.py
@@ -0,0 +1,42 @@
+'''
+Library for interacting with the PasteBin API
+https://pastebin.com/doc_api
+'''
+import requests
+
+PASTEBIN_API_POST_URL = 'https://pastebin.com/api/api_post.php'
+API_DEV_KEY = '3UH7-zh3G2aaVXWpNPgjYatXzEZORs4H'
+
+def post_new_paste(title, body_text, expiration='N', listed=True):
+    """Posts a new paste to PasteBin
+
+    Args:
+        title (str): Paste title
+        body_text (str): Paste body text
+        expiration (str): Expiration date of paste (N = never, 10M = minutes, 1H, 1D, 1W, 2W, 1M, 6M, 1Y)
+        listed (bool): Whether paste is publicly listed (True) or not (False) 
+    
+    Returns:
+        str: URL of new paste, if successful. Otherwise None.
+    """    
+    # Function body
+    # Note: This function will be written as a group 
+
+    print(" posting new paste to pastebin...", end= '')
+
+    post_params = {'api_dev_key': API_DEV_KEY,
+                   'api_option': 'paste' ,
+                   'api_paste_code': body_text,
+                   'api_paste_name': title,
+                   'api_paste_private' : 0 if listed else 1,
+                   'api_paste_expire_date': expiration}
+    # requeste a new paste bin paste
+    resp_msg =requests.post(PASTEBIN_API_POST_URL, data= post_params)
+    #check is paste was created successful
+    if resp_msg.status_code == requests.codes.ok:
+        print("Success!")
+    else:
+        print(f"Failure in creating paste!")
+        print(f'Status code: {resp_msg.status_code} ({resp_msg.reason})')
+
+    return resp_msg.text
---
+++ poke_api.py
@@ -0,0 +1,56 @@
+'''
+Library for interacting with the PokeAPI.
+https://pokeapi.co/
+'''
+import requests
+
+POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
+
+
+def main():
+    # Test out the get_pokemon_info() function
+    # Use breakpoints to view returned dictionary
+    pokemon_info = get_pokemon_info("Rockruff")
+    print(pokemon_info, end='\n\n')
+
+
+def get_pokemon_info(poke_name):
+    """Gets information about a specified Pokemon from the PokeAPI.
+
+    Args:
+        pokemon_name (str): Pokemon name (or Pokedex number)
+
+    Returns:
+        dict: Dictionary of Pokemon information, if successful. Otherwise None.
+    """
+    #Clean the Pokemon name parameter
+    poke_name = str(poke_name).strip().lower()
+
+    headers ={ 'Accept': 'Application /json'
+    }
+    get_params ={'entity': 'pokemon',
+                 'name':'Rockruff'
+    }    
+    #Build a clean URL and use it to send a GET request
+    print(f'Rockruff attack...', end= '')
+    url = 'https://pokeapi.co/api/v2/'
+    resp_msg = requests. get(url, params = get_params, headers=headers)
+    print(resp_msg.url)
+
+    #If the GET request was successful, convert the JSON-formatted message body text to a dictionary and return it
+    if resp_msg.status_code == requests.codes.ok:
+            print(f'Request Succesful!')
+            return resp_msg.json()
+    #If the GET request failed, print the error reason and return None
+    else: 
+            print(f'Request failed.')
+            print(f'Status code: {resp_msg.status_code} ({resp_msg.reason})')
+            return
+    
+    
+        
+    
+    return None
+
+if __name__ == '__main__':
+    main()
